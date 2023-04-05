54a209f94e700147127256ab957faa3bf0645fcc
Joël Riou <joel.riou@universite-paris-saclay.fr>
2023-04-05 15:22:44 +0000

    feat: port Order.Category.NonemptyFinLinOrd (#3282)
    Co-authored-by: Jeremy Tan Jie Rui <reddeloostw@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -1298,6 +1298,7 @@ import Mathlib.Order.Bounds.Basic
 import Mathlib.Order.Bounds.OrderIso
 import Mathlib.Order.Category.LatCat
 import Mathlib.Order.Category.LinOrdCat
+import Mathlib.Order.Category.NonemptyFinLinOrdCat
 import Mathlib.Order.Category.PartOrdCat
 import Mathlib.Order.Category.PreordCat
 import Mathlib.Order.Chain
