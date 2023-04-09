c5f5ae66a50f810092ae8a36b880a581fbaf1e42
MonadMaverick <MonadMaverick@pm.me>
2023-04-09 15:39:55 +1000

    automated fixes
    Mathbin -> Mathlib
    fix certain import statements
    move "by" to end of line
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -1294,6 +1294,7 @@ import Mathlib.Mathport.Syntax
 import Mathlib.MeasureTheory.CardMeasurableSpace
 import Mathlib.MeasureTheory.MeasurableSpace
 import Mathlib.MeasureTheory.MeasurableSpaceDef
+import Mathlib.MeasureTheory.Measure.NullMeasurable
 import Mathlib.MeasureTheory.PiSystem
 import Mathlib.ModelTheory.Basic
 import Mathlib.NumberTheory.ADEInequality
